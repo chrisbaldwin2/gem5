edf012f07f0f946440b79894ee9701011b945700
Chris <chris@ecewkgsw05201.engr.tamu.edu>
2023-10-31 12:29:22 -0500

    Change to spp prefetcher

--- src/python/gem5/components/cachehierarchies/classic/caches/l1dcache.py
+++ src/python/gem5/components/cachehierarchies/classic/caches/l1dcache.py
@@ -49,7 +49,7 @@ class L1DCache(Cache):
         mshrs: int = 16,
         tgts_per_mshr: int = 20,
         writeback_clean: bool = False,
-        PrefetcherCls: Type[MultiPrefetcher] = MultiPrefetcher,
+        PrefetcherCls: Type[BasePrefetcher] = SignaturePathPrefetcher,
     ):
         super().__init__()
         self.size = size
@@ -60,4 +60,6 @@ class L1DCache(Cache):
         self.mshrs = mshrs
         self.tgts_per_mshr = tgts_per_mshr
         self.writeback_clean = writeback_clean
-        self.prefetcher = PrefetcherCls(prefetchers=[BOPPrefetcher(use_virtual_addresses=True), IrregularStreamBufferPrefetcher(use_virtual_addresses=True), SignaturePathPrefetcher(use_virtual_addresses=True), StridePrefetcher(use_virtual_addresses=True)])
+        self.prefetcher = PrefetcherCls(prefetch_on_access=True, prefetch_on_pf_hit=True, use_virtual_addresses=True)
+        self.prefetch_on_access=True
+        self.prefetch_on_pf_hit=True
